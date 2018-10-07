82e6f44f0dd85fe4cada0ea6b33bde3cdeafa21a
Heitorado <hscs@cin.ufpe.br>
2018-10-06 23:22:56 -0300

    Implementa serviço de aluno injetado

--- gui/ess-ta-gui/src/app/aluno.service.ts
+++ gui/ess-ta-gui/src/app/aluno.service.ts
@@ -1,8 +1,10 @@
+import { Injectable, Inject } from '@angular/core';
 import { Aluno } from './aluno';
 
+@Injectable()
 export class AlunoService {
   alunos: Aluno[] = [];
-  
+
   create(aluno: Aluno): void {
      this.alunos.push(aluno);
   }
--- gui/ess-ta-gui/src/app/app.component.ts
+++ gui/ess-ta-gui/src/app/app.component.ts
@@ -11,8 +11,9 @@ import { AlunoService } from './aluno.service';
 })
 export class AppComponent {
   aluno: Aluno = {nome: "", cpf: "", email: "", git: ""};
-  alunoService = new AlunoService();
-  alunos: Aluno[] = [];
+  constructor(private alunoService: AlunoService) {}
+  alunos: Aluno[] = [{nome: "ayla", cpf: "123", email: "a@blbla", git: "ayl"},
+                      {nome: "Heitor", cpf: "31743287801", email: "hscs@cin.ufpe.br", git: "heitorado"}];
 
 
   create(alu: Aluno): void {
